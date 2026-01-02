8732e47bf111fff881d34bb5015afce4a63e1e3c
Marcel Kwiatkowski <marcel2215@icloud.com>
2026-01-02 21:14:07 +0100

    Change name

--- src/settings.ts
+++ src/settings.ts
@@ -6,4 +6,4 @@ export const PLATFORM_NAME = 'SuplaPlatform';
 /**
  * This must match the name of your plugin as defined the package.json
  */
-export const PLUGIN_NAME = 'homebridge-supla-plugin';
+export const PLUGIN_NAME = '@marcel2215/homebridge-supla-plugin';
